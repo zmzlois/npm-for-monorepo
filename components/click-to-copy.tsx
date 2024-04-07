dea5b124381ca16f87ecb352fa88ac9e886a896f
zmzlois <lois.z@comcord.vision>
2024-04-08 00:32:11 +0100

    click to copy utils

---
+++ components/click-to-copy.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+interface Props {
+    packageName: string;
+    command: string;
+    item: string;
+    flag: string;
+    arg: string;
+}
+export const ClickToCopy = ({props}:{ props: Props }) => {
+
+
+  function copy() {
+    const ele = document.getElementById("input")
+
+    if (!ele) {
+      
+      toast.error("We can't copy empty text");
+      return;
+    }
+    const text = ele.textContent;
+    navigator.clipboard.writeText(text!);
+
+    toast.success("Copied to clipboard");
+  }
+
+
+    const string = `${props.packageName} ${props.command} ${props.item} ${props.flag} ${props.arg}`;
+    return (
+        <code
+                id="code"
+                onClick={() => copy}
+                className="flex content-center items-center gap-4 text-center text-sm"
+              >
+                <span id="input">
+                    {string}
+                </span>
+                <Button onClick={copy} className="ml-2" variant={"outline"}>
+                  Copy
+                </Button>
+              </code>
+    )
+}
