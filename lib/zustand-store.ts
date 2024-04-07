e9321e41b6084cc4e0a1726d19e1f0baaf6fe4bd
zmzlois <lois.z@comcord.vision>
2024-04-08 00:33:43 +0100

    session storage

--- lib/zustand-store.ts
+++ lib/zustand-store.ts
@@ -1,25 +1,38 @@
-import { create } from 'zustand'
-import { persist, createJSONStorage } from 'zustand/middleware'
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const usePkgStore = create(
-    persist(
-        (set, get) => ({
-            packageManager: "pnpm",
-            command: "add",
-            setPackage: (item: "pnpm" | "yarn" | "npm") => {
-                if (item === "pnpm") set({ command: "add" })
-                if (item === "yarn") set({ command: "add" })
-                if (item === "npm") set({ command: "install" })
-                return set({ packageManager: item })
-            },
-            flag: "",
-            setFlag: (item: string) => set({ flag: item }),
-            arg: "",
-            setArg: (item: string) => set({ arg: item }),
-        }),
-        {
-            name: 'package', // name of the item in the storage (must be unique)
-            storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+type State = {
+    packageManager: "pnpm" | "yarn" | "npm";
+    command: "add" | "install";
+    flag: string;
+    arg: string;
+};
+
+type Actions = {
+    setPackage: (item: "pnpm" | "yarn" | "npm") => void;
+    setFlag: (item: string) => void;
+    setArg: (item: string) => void;
+};
+
+export const usePkgStore = create<State & Actions>()(
+
+    persist((set) => ({
+        packageManager: "pnpm",
+        command: "add",
+        setPackage: (item: "pnpm" | "yarn" | "npm") => {
+            if (item === "pnpm") set({ command: "add" });
+            if (item === "yarn") set({ command: "add" });
+            if (item === "npm") set({ command: "install" });
+            return set({ packageManager: item });
         },
-    ),
-)
+        flag: "",
+        setFlag: (item: string) => set({ flag: item }),
+        arg: "",
+        setArg: (item: string) => set({ arg: item }),
+    }),
+        {
+            name: "npm-for-monorepo", // name of the item in the storage (must be unique)
+            storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+        },)
+
+);
