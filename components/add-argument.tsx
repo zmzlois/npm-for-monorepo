bcf5a0936703e03fae8b712329a89aae5bbd6256
zmzlois <lois.z@comcord.vision>
2024-04-08 00:32:05 +0100

    edit argument

--- components/add-argument.tsx
+++ components/add-argument.tsx
@@ -11,15 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import {
   Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { InputField } from "./input.field";
 import z from "zod";
 import { useForm } from "react-hook-form";
