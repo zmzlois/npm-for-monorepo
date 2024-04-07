1123a3df0cb3bfd0a2b7495eae8d52062d5521cd
zmzlois <lois.z@comcord.vision>
2024-04-08 00:31:37 +0100

    main page refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-"use client"
-import Link from "next/link"
+"use client";
+import Link from "next/link";
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { SearchBar } from "@/components/search-bar"
-import { Toaster } from "@/components/ui/sonner"
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
+import { SearchBar } from "@/components/search-bar";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function IndexPage() {
   return (
-    <section className=" container mx-auto h-[calc(100vh-24vh)]  flex flex-col items-center justify-center gap-6 pb-8 pt-6 md:py-10">
+    <section className=" container mx-auto flex  h-[calc(100vh-24vh)] flex-col items-center justify-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex flex-col items-center gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Find any npm packages for monorepo<br className="hidden sm:inline" />
-          <code>pnpm add @vercel/analytics --filter web</code>
-        </h1>
-       
+        <h1 className="text-3xl font-extrabold text-cetner leading-tight tracking-tighter md:text-4xl">
+          Find any npm packages for monorepo
+          <br className="hidden sm:inline" />
+        </h1>  <code>pnpm add @vercel/analytics --filter web</code>
       </div>
-       <SearchBar showDescription={true} onNav={false}/>
-    <Toaster />
+      <SearchBar showDescription={true} onNav={false} />
     </section>
-  )
+  );
 }
