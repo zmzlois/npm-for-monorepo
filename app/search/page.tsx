42f22b6a6ac662191857325c1f3c48a4a1e3e995
zmzlois <lois.z@comcord.vision>
2024-04-08 01:42:44 +0100

    pagination support

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,20 +3,23 @@ import { useEffect, useState } from "react";
 import queryResult from "@/lib/query-npm";
 import { useSearchParams } from "next/navigation";
 import { usePkgStore } from "@/lib/zustand-store";
-import { ClickToCopy } from "@/components/click-to-copy";
-
+import { ClickToCopy } from "@/components/fields/click-to-copy";
+import { Pagination } from "@/components/fields/pagination";
 export default function Page() {
 
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false); 
+     const [result, setResult] = useState<string[]>([]);
+    
     const searchParams = useSearchParams();
  
     const search = searchParams.get("result") ?? "" ;
    
-     const page = searchParams.get("page") ?? 0;
+     let page = searchParams.get("page") ?? 0;
+
  
-     const pageNumber = parseInt(page as string);
- 
-     const [result, setResult] = useState<string[]>([]);
+     page = parseInt(page as string);
+    const [pageNumber, setPageNumber] = useState(page);
+   
 
      const args = usePkgStore((state: any) => {
         return {
@@ -33,11 +36,11 @@ export default function Page() {
              .then((res) => setResult(res))
              .catch((error) => console.log(error));
         
-     }, [])
+     }, [search, pageNumber])
     return (
-     
+     <div className="flex flex-col gap-4">
         <ul className="container mx-auto flex flex-col items-start px-20 py-5">
-           {result.length > 0 && result.map((item, index) => (
+           {result.length > 0 && result.slice(pageNumber, pageNumber+20).map((item, index) => (
             <li
               key={index}
               className="flex w-full content-center items-center justify-between border-b py-8 "
@@ -52,7 +55,8 @@ export default function Page() {
             </li>
           ))}
         </ul>
-    
+        {result.length > 20 && <Pagination page={pageNumber} setPage={setPageNumber} totalPage={result.length /20} />}
+    </div>
     );
   }
 
--- lib/query-npm.tsx
+++ lib/query-npm.tsx
@@ -7,14 +7,13 @@ export default async function queryResult(search: string, pageNumber?: number) {
 
     let page = 0;
 
-    console.log("search", search);
+
     if (pageNumber) page = pageNumber;
 
     result = search
       ? await Object.values(name)
           .filter((name) => name.includes(search) && name.length < 40)
-          .slice(page, page + 20)
-      : await Object.values(name).slice(page, page + 20);
+      : await Object.values(name);
   
    
     return result
